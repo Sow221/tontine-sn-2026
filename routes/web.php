ad1fa0f5c99c3108ed6cba9027b93e2589e441e1
Sow221 <sow221@tontine.sn>
2026-05-05 20:12:16 +0000

    Branchement de la vitrine

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', fn() => view('welcome'))->name('home');
+Route::get('/', fn() => view('landing'))->name('home');
 
 // Authentification (Magic Link)
 Route::middleware('guest')->group(function () {
